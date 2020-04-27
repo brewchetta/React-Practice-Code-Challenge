35ac73763630cf53d964c9ecd7c0242db968eb86
brewchetta <chett.tiller@gmail.com>
2020-04-27 11:18:24 -0400

    completed

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
-
-// Endpoint!
-const API = "http://localhost:3000/sushis"
+const PORT = 3000
+const API = `http://localhost:${PORT}/sushis`
+const fetchSushis = () => fetch(API).then(res => res.json())
 
 class App extends Component {
+
+  state = {
+    sushis: [],
+    money: 100
+  }
+
+  componentDidMount = () => fetchSushis().then(sushis => this.setState({sushis}))
+
+  eatSushi = sushi => {
+    if (sushi.price <= this.state.money) {
+      const sushis = [...this.state.sushis]
+      sushis.splice(sushi.id - 1, 1, {...sushi, eaten: true})
+      this.setState({sushis, money: this.state.money - sushi.price})
+    } else {
+      alert(`${sushi.name} is out of your price range!`)
+    }
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={this.state.sushis} eatSushi={this.eatSushi} />
+        <Table plates={this.state.sushis.filter(s => s.eaten)} money={this.state.money} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,28 @@
-import React, { Fragment } from 'react'
+import React, { useState, Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
+const SushiContainer = ({sushis, eatSushi}) => {
+
+  const [page, setPage] = useState(0)
+
+  const renderSushis = () => {
+    return sushis.slice(page * 4, page * 4 + 4).map(sushi => <Sushi key={sushi.id} eatSushi={eatSushi} sushi={sushi} />)
+  }
+
+  const moreSushis = () => {
+    if ((page - 1) * 4 >= sushis.length) {
+      setPage(0)
+    } else {
+      setPage(page + 1)
+    }
+  }
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {renderSushis()}
+        <MoreButton moreSushis={moreSushis} />
       </div>
     </Fragment>
   )
